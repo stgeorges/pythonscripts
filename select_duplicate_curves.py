fe60dc864367f65dc93616b8f3e6b01332d43db2
Djordje Spasic <issworld2000@yahoo.com>
2020-02-13 09:41:06 +0100

    Speeding up the function

--- select_duplicate_curves.py
+++ select_duplicate_curves.py
@@ -1,7 +1,7 @@
 #********************************************************************************************#
 #********* Select duplicate curves **********************************************************#
 #********* by Djordje Spasic ****************************************************************#
-#********* issworld2000@yahoo.com 16-May-2014 ***********************************************#
+#********* issworld2000@yahoo.com 16-May-2014. Rev01 13.02.2020 *****************************#
 
 """
 Function selects all duplicate curves in document, whether they're positioned on top of each other 
@@ -11,13 +11,14 @@ import rhinoscriptsyntax as rs
 import scriptcontext as sc
 import Rhino
 
-def cullDuplicateCurves(_ids, _t, _tol, _flippedCrvs, _overlapped):
+def cullDuplicateCurves(_ids, _tol, _flippedCrvs, _overlapped):
     if len(_ids) > 1:
+        _t = 100000000
         objs = [sc.doc.Objects.Find(id) for id in _ids]
         nc_objs = [crv.Geometry.ToNurbsCurve() for crv in objs]
         indx = []
-        for i in range(len(nc_objs)):
-            for k in range(len(nc_objs)):
+        for i in xrange(len(nc_objs)):
+            for k in xrange(len(nc_objs)):
                 if (i != k) and (i not in indx) and (k not in indx) and (round(nc_objs[i].GetLength(), 3) == round(nc_objs[k].GetLength(), 3)):
                         if nc_objs[i].EpsilonEquals(nc_objs[k], _t):
                             crv1TVst = nc_objs[i].TangentAt(nc_objs[i].Domain[0])
@@ -36,12 +37,12 @@ def cullDuplicateCurves(_ids, _t, _tol, _flippedCrvs, _overlapped):
                             crv2CVmid2 = nc_objs[i].CurvatureAt(crv2t2)
                             crv2CVend = nc_objs[k].CurvatureAt(nc_objs[k].Domain[1])
                             crv2TVend = nc_objs[k].TangentAt(nc_objs[k].Domain[1])
-
+                            
                             # Select duplicated curves scattered around the document and overlapped. Flipped ones will not be included.
                             if _flippedCrvs == "No" and _overlapped == "No":
                                 if ( crv1TVst.EpsilonEquals(crv2TVst, _tol) and crv1CVst.EpsilonEquals(crv2CVst, _tol) and crv1CVmid1.EpsilonEquals(crv2CVmid1, _tol) and crv1CVmid2.EpsilonEquals(crv2CVmid2, _tol) and crv1CVend.EpsilonEquals(crv2CVend, _tol) and crv1TVend.EpsilonEquals(crv2TVend, _tol) ):
                                     indx.append(i)
-
+                            
                             # Select duplicated curves scattered around the document and overlapped. Flipped ones included too.
                             elif _flippedCrvs == "Yes" and _overlapped == "No":
                                 crv2TVstR = Rhino.Geometry.Vector3d(crv2TVst)
@@ -62,7 +63,7 @@ def cullDuplicateCurves(_ids, _t, _tol, _flippedCrvs, _overlapped):
                                 or ( crv1TVst.EpsilonEquals(crv2TVendR, _tol) and crv1CVst.EpsilonEquals(crv2CVend, _tol) and crv1CVmid1.EpsilonEquals(crv2CVmid2, _tol) and crv1CVmid2.EpsilonEquals(crv2CVmid1, _tol) and crv1CVend.EpsilonEquals(crv2CVst, _tol) and crv1TVend.EpsilonEquals(crv2TVstR, _tol) )\
                                 or ( crv1TVst.EpsilonEquals(crv2TVstRR, _tol) and crv1CVst.EpsilonEquals(crv2CVstR, _tol) and crv1CVmid1.EpsilonEquals(crv2CVmid1R, _tol) and crv1CVmid2.EpsilonEquals(crv2CVmid2R, _tol) and crv1CVend.EpsilonEquals(crv2CVendR, _tol) and crv1TVend.EpsilonEquals(crv2TVendRR, _tol) ):
                                     indx.append(i)
-
+                            
                             # Select only overlapped duplicated curves. Flipped ones will not be included.
                             elif _flippedCrvs == "No" and _overlapped == "Yes":
                                 t1 = (nc_objs[i].Domain[0]+nc_objs[i].Domain[1])/2
@@ -71,7 +72,7 @@ def cullDuplicateCurves(_ids, _t, _tol, _flippedCrvs, _overlapped):
                                 midCrv2 = nc_objs[k].PointAt(t2)
                                 if ( midCrv1.EpsilonEquals(midCrv2, _tol) and crv1TVst.EpsilonEquals(crv2TVst, _tol) and crv1CVst.EpsilonEquals(crv2CVst, _t) and crv1CVmid1.EpsilonEquals(crv2CVmid1, _tol) and crv1CVmid2.EpsilonEquals(crv2CVmid2, _tol) and crv1CVend.EpsilonEquals(crv2CVend, _tol) and crv1TVend.EpsilonEquals(crv2TVend, _tol) ):
                                     indx.append(i)
-
+                            
                             # Select only overlapped duplicated curves. Flipped ones will be included too.
                             elif _flippedCrvs == "Yes" and _overlapped == "Yes":
                                 crv2TVstR = Rhino.Geometry.Vector3d(crv2TVst)
@@ -96,21 +97,23 @@ def cullDuplicateCurves(_ids, _t, _tol, _flippedCrvs, _overlapped):
                                 or ( midCrv1.EpsilonEquals(midCrv2, _tol) and crv1TVst.EpsilonEquals(crv2TVendR, _tol) and crv1CVst.EpsilonEquals(crv2CVend, _tol) and crv1CVmid1.EpsilonEquals(crv2CVmid2, _tol) and crv1CVmid2.EpsilonEquals(crv2CVmid1, _tol) and crv1CVend.EpsilonEquals(crv2CVst, _tol) and crv1TVend.EpsilonEquals(crv2TVstR, _tol) )\
                                 or ( midCrv1.EpsilonEquals(midCrv2, _tol) and crv1TVst.EpsilonEquals(crv2TVstRR, _tol) and crv1CVst.EpsilonEquals(crv2CVstR, _tol) and crv1CVmid1.EpsilonEquals(crv2CVmid1R, _tol) and crv1CVmid2.EpsilonEquals(crv2CVmid2R, _tol) and crv1CVend.EpsilonEquals(crv2CVendR, _tol) and crv1TVend.EpsilonEquals(crv2TVendRR, _tol) ):
                                     indx.append(i)
-
-        for index in indx:
-            rs.SelectObject(ids[index])
+        
+        rs.EnableRedraw(False)
+        [rs.SelectObject(ids[index])  for index in indx]
+        rs.EnableRedraw(True)
+        
         if len(indx) > 0:
             print "Found %s duplicate curves" % len(indx)
         else:
             print "No duplicate curves found"
 
 
-t = 100000000
+
 tol = sc.doc.ModelAbsoluteTolerance
 ids = rs.GetObjects("Pick up the curves you would like to clean from duplicates", 4, preselect=True)
 if ids != None:
     flippedCrvs = rs.GetString("Consider flipped curves too?", "No", ["Yes" , "No"])
     overlapped = rs.GetString("Consider only overlapped (not those scattered around) curves?", "No", ["Yes" , "No"])
-    cullDuplicateCurves(ids, t, tol, flippedCrvs, overlapped)
+    cullDuplicateCurves(ids, tol, flippedCrvs, overlapped)
 else:
     print "No curves selected"
